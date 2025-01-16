8e2d1d228f1e0beefec5e08371924d7e443dda93
predrag jandric <predrag.jandric.bg@gmail.com>
2025-01-16 23:21:04 +0100

    will ADD: useReducer cases logic for the quiz

---
+++ src/components/Quiz.jsx
@@ -0,0 +1,64 @@
+import { useReducer } from "react";
+import data from "./quizQuestions.json";
+
+const shuffledQuestions = data.sort(() => Math.random() - 0.5);
+
+const initialState = {
+  questions: shuffledQuestions,
+  status: "active",
+  index: 0,
+  answer: null,
+  points: 0,
+  highscore: 0,
+};
+
+function reducer(state, action) {
+  switch (action.type) {
+    case "newAnswer":
+      const question = state.questions.at(state.index);
+
+      return {
+        ...state,
+        answer: action.payload,
+        points:
+          action.payload === question.correctOption
+            ? state.points + question.points
+            : state.points,
+      };
+
+    case "nextQuestion":
+      return { ...state, index: state.index + 1, answer: null };
+
+    case "finish":
+      return {
+        ...state,
+        status: "finished",
+        highscore:
+          state.points > state.highscore ? state.points : state.highscore,
+      };
+
+    case "restart":
+      return {
+        ...state,
+        status: "active",
+        index: 0,
+        answer: null,
+        points: 0,
+      };
+
+    default:
+      throw new Error("action unknow");
+  }
+}
+
+function App() {
+ 
+
+  return (
+    <div >
+     test
+    </div>
+  );
+}
+
+export default App;
