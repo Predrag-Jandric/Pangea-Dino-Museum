cb8ef9be88fb1d34a986d9f2ccf4f0856cb23d00
predrag jandric <predrag.jandric.bg@gmail.com>
2025-01-16 23:16:30 +0100

    will ADD: data for the quiz

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,11 @@
-import Footer from "./components/Footer";
 import LandingPage from "./components/LandingPage";
 import Test from "./components/Test";
 
 function App() {
   return (
     <div className="">
-      {/* <LandingPage /> */}
-      {/* <Test /> */}
-      <Footer />
+      <LandingPage />
+      <Test />
     </div>
   );
 }
--- src/utils/data.js
+++ src/utils/data.js
@@ -11,3 +11,87 @@ export const landingPageBGimageSlider = [
   "https://img.freepik.com/premium-photo/three-adorable-dinosaurs-by-river_107062-22159.jpg",
   "https://img.freepik.com/premium-photo/two-friendly-triceratops-enjoying-day_107062-22168.jpg",
 ];
+
+export const data = [
+  {
+    question: "Which dinosaur's name means 'tyrant lizard king'?",
+    options: [
+      "Triceratops",
+      "Tyrannosaurus Rex",
+      "Velociraptor",
+      "Stegosaurus",
+    ],
+    correctOption: 1,
+    points: 10,
+  },
+  {
+    question: "What was the main diet of a Brachiosaurus?",
+    options: ["Insects", "Meat", "Plants", "Fish"],
+    correctOption: 2,
+    points: 10,
+  },
+  {
+    question: "Which dinosaur had three horns on its head?",
+    options: ["Triceratops", "Diplodocus", "Spinosaurus", "Ankylosaurus"],
+    correctOption: 0,
+    points: 10,
+  },
+  {
+    question: "Which dinosaur was known for the large plates along its back?",
+    options: ["Allosaurus", "Stegosaurus", "Pachycephalosaurus", "Gallimimus"],
+    correctOption: 1,
+    points: 10,
+  },
+  {
+    question:
+      "Which dinosaur was one of the fastest, reaching speeds up to 40 mph?",
+    options: [
+      "Tyrannosaurus Rex",
+      "Velociraptor",
+      "Hadrosaurus",
+      "Compsognathus",
+    ],
+    correctOption: 3,
+    points: 10,
+  },
+  {
+    question: "What is the term for a scientist who studies fossils?",
+    options: ["Archaeologist", "Paleontologist", "Geologist", "Biologist"],
+    correctOption: 1,
+    points: 10,
+  },
+  {
+    question: "Which dinosaur had a long tail that could be used as a whip?",
+    options: ["Apatosaurus", "Stegosaurus", "Brachiosaurus", "Iguanodon"],
+    correctOption: 0,
+    points: 10,
+  },
+  {
+    question: "Which dinosaur is known for having a large sail on its back?",
+    options: ["Spinosaurus", "Ankylosaurus", "Parasaurolophus", "Oviraptor"],
+    correctOption: 0,
+    points: 10,
+  },
+  {
+    question: "When did dinosaurs go extinct?",
+    options: [
+      "65 million years ago",
+      "100 million years ago",
+      "50 million years ago",
+      "25 million years ago",
+    ],
+    correctOption: 0,
+    points: 10,
+  },
+  {
+    question: "What is believed to have caused the extinction of dinosaurs?",
+    options: [
+      "Volcanic eruptions",
+      "A meteor impact",
+      "Climate change",
+      "A massive flood",
+    ],
+    correctOption: 1,
+    points: 10,
+  },
+];
