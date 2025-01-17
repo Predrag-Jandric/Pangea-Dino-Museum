fc9cfb35542ba28f68202b22272e1aff04c6f2e8
predrag jandric <predrag.jandric.bg@gmail.com>
2025-01-17 20:23:22 +0100

    will ADD: styling for timeline component  and i did testing to make sure everything works, which it does

--- src/components/Timeline.jsx
+++ src/components/Timeline.jsx
@@ -17,15 +17,19 @@ function Timeline() {
     );
   };
   return (
-    <div>
-      <div>
-        <button onClick={handlePrev}>
+    <div className="flex flex-col items-center py-32 p-8 bg-[#F7F7F7]">
+      <div className="flex items-center justify-between w-full max-w-4xl">
+        <button
+          onClick={handlePrev}
+          className="p-3 rounded-full border-2 border-grayOne text-lg  hover:border-primary transition-all hover:text-primary"
+        >
           <IoIosArrowBack />
         </button>
 
-        <div>
-          <div>
+        <div className="relative flex-1 mx-4">
+          <div className="absolute top-1/2 left-0 right-0 h-1 bg-grayOne transform -translate-y-1/2">
             <div
+              className="h-1 bg-primary transition-all duration-500"
               style={{
                 width: `${
                   (selectedIndex / (timelineEvents.length - 1)) * 100
@@ -34,26 +38,49 @@ function Timeline() {
             ></div>
           </div>
 
-          <div>
+          <div className="flex justify-between items-center relative z-10">
             {timelineEvents.map((event, index) => (
-              <div key={index} onClick={() => setSelectedIndex(index)}>
-                <span>{event.title}</span>
+              <div
+                key={index}
+                onClick={() => setSelectedIndex(index)}
+                className="relative  py-1 cursor-pointer flex flex-col items-center"
+              >
+                <span
+                  className={`absolute -top-14 p-2 text-center w-23 text-sm font-semibold  ${
+                    index === selectedIndex ? "text-primary" : "text-gray-500"
+                  }`}
+                >
+                  {event.title}
+                </span>
 
-                <div></div>
+                <div
+                  className={`w-5 h-5 rounded-full ${
+                    index <= selectedIndex
+                      ? "bg-primary delay-300 border-primary"
+                      : "bg-[#F7F7F7] delay-0 border-grayOne hover:bg-primary"
+                  } border-2 transform -translate-y-0 transition-colors`}
+                ></div>
               </div>
             ))}
           </div>
         </div>
 
-        <button onClick={handleNext}>
+        <button
+          onClick={handleNext}
+          className="p-3 rounded-full border-2 border-grayOne text-lg  hover:border-primary transition-all hover:text-primary"
+        >
           <IoIosArrowForward />
         </button>
       </div>
 
-      <div>
-        <h2>{timelineEvents[selectedIndex].title}</h2>
+      <div className="mt-10 p-6 flex flex-col gap-4 w-full max-w-4xl">
+        <h2 className="text-6xl text-gray-800">
+          {timelineEvents[selectedIndex].title}
+        </h2>
         <p className="italic">- {timelineEvents[selectedIndex].time}</p>
-        <p>{timelineEvents[selectedIndex].description}</p>
+        <p className="mt-4 text-lg text-gray-600">
+          {timelineEvents[selectedIndex].description}
+        </p>
       </div>
     </div>
   );
