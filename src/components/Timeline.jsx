318d16b81e01e55e47621a8f65fc7b87264d33ac
predrag jandric <predrag.jandric.bg@gmail.com>
2025-01-17 20:20:43 +0100

    will ADD: unstyled jsx for timeline component

--- src/components/Timeline.jsx
+++ src/components/Timeline.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import { useState } from "react";
 import { IoIosArrowBack, IoIosArrowForward } from "react-icons/io";
 import { timelineEvents } from "../utils/data";
 
@@ -7,7 +7,7 @@ function Timeline() {
 
   const handleNext = () => {
     setSelectedIndex((prevIndex) =>
-      prevIndex === events.length - 1 ? 0 : prevIndex + 1
+      prevIndex === timelineEvents.length - 1 ? 0 : prevIndex + 1
     );
   };
 
@@ -16,7 +16,47 @@ function Timeline() {
       prevIndex === 0 ? timelineEvents.length - 1 : prevIndex - 1
     );
   };
-  return <div>test</div>;
+  return (
+    <div>
+      <div>
+        <button onClick={handlePrev}>
+          <IoIosArrowBack />
+        </button>
+
+        <div>
+          <div>
+            <div
+              style={{
+                width: `${
+                  (selectedIndex / (timelineEvents.length - 1)) * 100
+                }%`,
+              }}
+            ></div>
+          </div>
+
+          <div>
+            {timelineEvents.map((event, index) => (
+              <div key={index} onClick={() => setSelectedIndex(index)}>
+                <span>{event.title}</span>
+
+                <div></div>
+              </div>
+            ))}
+          </div>
+        </div>
+
+        <button onClick={handleNext}>
+          <IoIosArrowForward />
+        </button>
+      </div>
+
+      <div>
+        <h2>{timelineEvents[selectedIndex].title}</h2>
+        <p className="italic">- {timelineEvents[selectedIndex].time}</p>
+        <p>{timelineEvents[selectedIndex].description}</p>
+      </div>
+    </div>
+  );
 }
 
 export default Timeline;
