c7e787a8b6684314bb1b34c0ccc4228ef648684c
Greg <gregminezzi@gmail.com>
2025-01-18 19:21:41 +0100

    initial dinoDisplay setup

---
+++ src/components/DinoDisplay.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function DinoDisplay() {
+  return (
+    <div>dinoDisplay</div>
+  )
+}
---
+++ src/components/DinoInfoCard.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function DinoInfoCard() {
+  return (
+    <div>dinoInfoCard</div>
+  )
+}
