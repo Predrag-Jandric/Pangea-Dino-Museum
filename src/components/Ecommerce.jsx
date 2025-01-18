c56e7c1241e2374d7c290b54bd5b4c179ba4065b
predrag jandric <predrag.jandric.bg@gmail.com>
2025-01-18 22:23:34 +0100

    will ADD: temporary data to be mapped over in the ecommerce component shop

---
+++ src/components/Ecommerce.jsx
@@ -0,0 +1,113 @@
+const fakeData = [
+  {
+    id: 2,
+    name: "Abelisaurus",
+    imageSrc:
+      "https://images.dinosaurpictures.org/Abelisaurus%2Bcomahuensis6_0373.jpg",
+    typeOfDinosaur: "large theropod",
+    diet: "carnivorous",
+    whenLived: "Late Cretaceous, 74-70 million years ago",
+    foundIn: "Argentina",
+    taxonomy:
+      "Dinosauria, Saurischia, Theropoda, Neotheropoda, Ceratosauria, Abelisauridae, Abelisaurinae",
+    namedBy: "Bonaparte and Novas (1985)",
+    typeSpecies: "comahuensis",
+    description: "N/A",
+  },
+  {
+    id: 3,
+    name: "Achelousaurus",
+    imageSrc:
+      "https://images.dinosaurpictures.org/achelousaurus_by_deskridge-d7mncn5_cefb.jpg",
+    typeOfDinosaur: "ceratopsian",
+    diet: "herbivorous",
+    whenLived: "Late Cretaceous, 83-70 million years ago",
+    foundIn: "USA",
+    taxonomy:
+      "Dinosauria, Ornithischia, Neornithischia, Cerapoda, Marginocephalia, Ceratopsia, Neoceratopsia, Coronosauria, Ceratopsidae, Centrosaurinae",
+    namedBy: "Sampson  (1995)",
+    typeSpecies: "horneri",
+    description: "N/A",
+  },
+  {
+    id: 6,
+    name: "Aegyptosaurus",
+    imageSrc:
+      "https://images.dinosaurpictures.org/Collab__Aegyptosaurus_by_Eco727_c7a5.jpg",
+    typeOfDinosaur: "sauropod",
+    diet: "herbivorous",
+    whenLived: "Late Cretaceous, 98-93 million years ago",
+    foundIn: "Egypt",
+    taxonomy:
+      "Dinosauria, Saurischia, Sauropodomorpha, Sauropoda, Eusauropoda, Neosauropoda, Macronaria, Camarasauromorpha, Titanosauriformes, Titanosauria",
+    namedBy: "Stromer (1932)",
+    typeSpecies: "baharijensis",
+    description: "N/A",
+  },
+  {
+    id: 7,
+    name: "Afrovenator",
+    imageSrc:
+      "https://images.dinosaurpictures.org/Afrovenator_abakensis_dinosaur_802f.jpg",
+    typeOfDinosaur: "large theropod",
+    diet: "carnivorous",
+    whenLived: "Early Cretaceous, 132-121 million years ago",
+    foundIn: "Niger",
+    taxonomy:
+      "Dinosauria, Saurischia, Theropoda, Megalosauridae, Eustreptospondylinae",
+    namedBy: "Sereno, Wilson, Larsson, Dutheil and Sues (1944)",
+    typeSpecies: "abakensis",
+    description: "N/A",
+  },
+  {
+    id: 9,
+    name: "Alamosaurus",
+    imageSrc: "https://images.dinosaurpictures.org/alamosaurusDB_f460.jpg",
+    typeOfDinosaur: "sauropod",
+    diet: "herbivorous",
+    whenLived: "Late Cretaceous, 70-66 million years ago",
+    foundIn: "USA",
+    taxonomy:
+      "Dinosauria, Saurischia, Sauropodomorpha, Sauropoda, Eusauropoda, Neosauropoda, Macronaria, Camarasauromorpha, Titanosauriformes, Titanosauria, Lithostrotia",
+    namedBy: "Gilmore (1922)",
+    typeSpecies: "sanjuanensis",
+    description:
+      "This is the only Late Cretaceous sauropod that has been found in North America.",
+  },
+  {
+    id: 11,
+    name: "Albertosaurus",
+    imageSrc:
+      "https://images.dinosaurpictures.org/500px-Albertosaurus-Todd-Marshall_516e.jpg",
+    typeOfDinosaur: "large theropod",
+    diet: "carnivorous",
+    whenLived: "Late Cretaceous, 76-74 million years ago",
+    foundIn: "Canada",
+    taxonomy:
+      "Dinosauria, Saurischia, Theropoda, Tyrannosauroidea, Tyrannosauridae, Albertosaurinae",
+    namedBy: "Lamb (1914)",
+    typeSpecies: "libratus",
+    description:
+      "Albertosaurus was a close relative of Tyrannosaurus, but smaller and not as heavily built.",
+  },
+  {
+    id: 12,
+    name: "Alectrosaurus",
+    imageSrc:
+      "https://images.dinosaurpictures.org/Alectrosaurus-Gabriel-Lio_c279.jpg",
+    typeOfDinosaur: "large theropod",
+    diet: "carnivorous",
+    whenLived: "Late Cretaceous, 90-70 million years ago",
+    foundIn: "China, Mongolia",
+    taxonomy: "Dinosauria, Saurischia, Theropoda, Tyrannosauroidea",
+    namedBy: "Gilmore (1933)",
+    typeSpecies: "olseni",
+    description: "N/A",
+  },
+];
+
+function Ecommerce() {
+  return <div></div>;
+}
+
+export default Ecommerce;
