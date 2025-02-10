1f44b5ed216f4e779a880e9863944ad0948a2cee
Greg <gregminezzi@gmail.com>
2025-02-10 20:49:19 +0100

    get random number of news stories

--- src/components/News.jsx
+++ src/components/News.jsx
@@ -5,13 +5,14 @@ export default function News() {
   const NEWS_API_KEY = import.meta.env.VITE_NEWS_API_KEY;
   const NEWS_API_URL = import.meta.env.VITE_NEWS_API_URL;
 
+  // get news from API
   useEffect(() => {
     async function getNews() {
       try {
         const res = await fetch(`${NEWS_API_URL}/everything?q=tesla&from=2025-01-10&sortBy=publishedAt&apiKey=${NEWS_API_KEY}`);
         const data = await res.json();
 
-        if (data.articles) setNews(data.articles);
+        if (data.articles) setNews(getRandomArticle(data.articles, 3));
         else console.error("error fetching news: ", error);
       } catch (error) {
         console.error("Failed to fetch news:", error);
@@ -19,6 +20,12 @@ export default function News() {
     }
     getNews();
   }, []);
+
+  // get a random selection of articles, based on count passed
+  function getRandomArticle(articles, count) {
+    const randomized = [...articles].sort(() => .5 - Math.random());
+    return randomized.slice(0, count);
+  }
 
   return (
     <div className="h-dvh p-6 flex gap-3 flex-col items-center justify-center text-center bg-dark">
