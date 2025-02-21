949feaa0f51fb5d7803b19c2c03fe22f46873eab
predrag jandric <predrag.jandric.bg@gmail.com>
2025-02-21 17:18:02 +0100

    will REFACTOR: the styling of Quiz component

--- frontend/src/components/Quiz.jsx
+++ frontend/src/components/Quiz.jsx
@@ -76,120 +76,112 @@ function Quiz() {
   return (
     <div
       id="quiz"
-      className="bg-dark flex-colh-dvh flex min-h-screen flex-col items-center justify-center gap-7 lg:gap-0 p-6 text-center"
+      className="bg-dark flex-colh-dvh flex min-h-screen flex-col items-center justify-center gap-7 p-6 text-center"
     >
       <h2 className="font-pressStart text-4xl text-primary">Dino Quiz</h2>
-      <p className="text-light max-w-2xl my-3">
+      <p className="text-light max-w-2xl">
         Enter your mind palace and see if you still remember all those dino
         facts you learned!
       </p>
-        <p>* enter full screen for a treat!</p>
-
-      <img src="/gameboy/top.png" className=" w-[796px] ml-1 hidden lg:block" />
-
-      <div className="flex">
-        <img src="/gameboy/left.png" className=" w-[225px] hidden lg:block" />
-
-        <div className="border-secondary h-fit w-full max-w-2xl lg:w-[405px] lg:h-[390px] rounded-xl border-[5px] p-8 lg:p-2 shadow-lg">
-          {status === "active" && (
-            <section>
-              {/* progress bar */}
-              <div className="mb-6 lg:mb-2">
-                <progress
-                  className="[&::-webkit-progress-value]:bg-highlight [&::-webkit-progress-bar]:bg-dark [&::-webkit-progress-bar]:outline-secondary w-full [&::-webkit-progress-bar]:rounded-lg [&::-webkit-progress-bar]:outline [&::-webkit-progress-value]:rounded-lg"
-                  max={numQuestions}
-                  value={index + Number(answer !== null)}
-                />
-                <div className="text-secondary mt-2 flex justify-between text-lg font-medium lg:text-sm">
-                  <p>
-                    Question <span>{index + 1}</span> / {numQuestions}
-                  </p>
-                  <p>
-                    Points: <span>{points}</span> / {maxPossiblePoints}
-                  </p>
-                </div>
+      <div className="border-secondary h-fit w-full max-w-2xl rounded-xl border-[5px] p-8 shadow-lg">
+        {status === "active" && (
+          <section>
+            {/* progress bar */}
+            <div className="mb-6">
+              <progress
+                className="[&::-webkit-progress-value]:bg-highlight [&::-webkit-progress-bar]:bg-dark [&::-webkit-progress-bar]:outline-secondary w-full [&::-webkit-progress-bar]:rounded-lg [&::-webkit-progress-bar]:outline [&::-webkit-progress-value]:rounded-lg"
+                max={numQuestions}
+                value={index + Number(answer !== null)}
+              />
+              <div className="text-secondary mt-2 flex justify-between text-lg font-medium">
+                <p>
+                  Question <span>{index + 1}</span> / {numQuestions}
+                </p>
+                <p>
+                  Points: <span>{points}</span> / {maxPossiblePoints}
+                </p>
               </div>
-              {/* current question */}
-              <div className="mb-6 lg:mb-1">
-                <h4 className="text-highlight text-2xl font-semibold lg:text-sm">
-                  {currentQuestion.question}
-                </h4>
-              </div>
-              {/* answer options */}
-              <div className="space-y-4 lg:space-y-4">
-                {currentQuestion.options.map((option, optionIndex) => (
-                  <button
-                    key={option}
-                    onClick={() =>
-                      dispatch({ type: "newAnswer", payload: optionIndex })
-                    }
-                    className={`text-light w-full rounded-lg border-2 px-4 py-3 lg:p-2 text-left font-medium lg:text-sm ${
-                      // highlight selected answer
-                      optionIndex === answer
-                        ? optionIndex === currentQuestion.correctOption
-                          ? "bg-highlight border-highlight" // correct selected answer
-                          : "bg-secondary border-secondary" // incorrect selected answer
-                        : "bg-dark border-secondary text-light hover:border-light"
-                    } ${
-                      // highlight correct answer when an incorrect answer is selected
-                      answer !== null &&
-                      optionIndex === currentQuestion.correctOption &&
-                      optionIndex !== answer
-                        ? "bg-highlight border-highlight"
-                        : ""
-                    } ${
-                      // dim non-selected options after answering
-                      answer !== null && optionIndex !== answer
-                        ? "opacity-100"
-                        : ""
-                    }`}
-                    disabled={answer !== null} // disable buttons after answering
-                  >
-                    {option}
-                  </button>
-                ))}
-              </div>
-              {/* next/finish button */}
-       
-                <button
-                  onClick={() =>
-                    dispatch({
-                      type:
-                        index < numQuestions - 1 ? "nextQuestion" : "finish",
-                    })
-                  }
-                  className={`text-light hover:bg-highlight mt-6 lg:mt-3 w-full rounded-lg bg-primary py-2 ${answer === null && "disable bg-secondary/50" }`}
-                >
-                  {index < numQuestions - 1 ? "Next" : "Finish"}
-                </button>
-           
-            </section>
-          )}
-          {/* finished State */}
-          {status === "finished" && (
-            <div className="text-center">
-              <p className="text-highlight mb-4 text-3xl font-bold">
-                <span>{emoji}</span> You scored <span>{points}</span> out of{" "}
-                {maxPossiblePoints} ({Math.ceil(percentage)}%)
-              </p>
-              <p className="text-secondary mb-6 text-lg">
-                Highscore: {highscore} points
-              </p>
-              <button
-                onClick={() => dispatch({ type: "restart" })}
-                className="text-light hover:bg-highlight rounded-lg bg-primary px-6 py-3 transition-all"
-              >
-                Restart Quiz
-              </button>
             </div>
-          )}
-        </div>
-        <img src="/gameboy/right.png" className="w-[220px] hidden lg:block" />
+
+            {/* current question */}
+            <div className="mb-6">
+              <h4 className="text-highlight text-2xl font-semibold">
+                {currentQuestion.question}
+              </h4>
+            </div>
+
+            {/* answer options */}
+            <div className="space-y-4">
+              {currentQuestion.options.map((option, optionIndex) => (
+                <button
+                  key={option}
+                  onClick={() =>
+                    dispatch({ type: "newAnswer", payload: optionIndex })
+                  }
+                  className={`text-light w-full rounded-lg border-2 px-4 py-3 text-left font-medium ${
+                    // highlight selected answer
+                    optionIndex === answer
+                      ? optionIndex === currentQuestion.correctOption
+                        ? "bg-highlight border-highlight" // correct selected answer
+                        : "bg-secondary border-secondary" // incorrect selected answer
+                      : "bg-dark border-secondary text-light hover:border-light"
+                  } ${
+                    // highlight correct answer when an incorrect answer is selected
+                    answer !== null &&
+                    optionIndex === currentQuestion.correctOption &&
+                    optionIndex !== answer
+                      ? "bg-highlight border-highlight"
+                      : ""
+                  } ${
+                    // dim non-selected options after answering
+                    answer !== null && optionIndex !== answer
+                      ? "opacity-100"
+                      : ""
+                  }`}
+                  disabled={answer !== null} // disable buttons after answering
+                >
+                  {option}
+                </button>
+              ))}
+            </div>
+
+            {/* next/finish button */}
+            {answer !== null && (
+              <button
+                onClick={() =>
+                  dispatch({
+                    type: index < numQuestions - 1 ? "nextQuestion" : "finish",
+                  })
+                }
+                className="text-light hover:bg-highlight mt-6 w-full rounded-lg bg-primary py-3"
+              >
+                {index < numQuestions - 1 ? "Next" : "Finish"}
+              </button>
+            )}
+          </section>
+        )}
+
+        {/* finished State */}
+        {status === "finished" && (
+          <div className="text-center">
+            <p className="text-highlight mb-4 text-3xl font-bold">
+              <span>{emoji}</span> You scored <span>{points}</span> out of{" "}
+              {maxPossiblePoints} ({Math.ceil(percentage)}%)
+            </p>
+
+            <p className="text-secondary mb-6 text-lg">
+              Highscore: {highscore} points
+            </p>
+
+            <button
+              onClick={() => dispatch({ type: "restart" })}
+              className="text-light hover:bg-highlight rounded-lg bg-primary px-6 py-3 transition-all"
+            >
+              Restart Quiz
+            </button>
+          </div>
+        )}
       </div>
-      <img
-        src="/gameboy/bottom.png"
-        className=" w-[801px] ml-[9px] hidden lg:block"
-      />
     </div>
   );
 }
