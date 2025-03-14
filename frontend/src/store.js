ba67450319585d4478c0a5f3138c67a78c19249c
predrag jandric <predrag.jandric.bg@gmail.com>
2025-03-15 00:18:23 +0100

    will ADD: local storage

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,10 +1,22 @@
-import {configureStore} from '@reduxjs/toolkit';
-import shoppingReducer from "./utils/shoppingSlice"
+import { configureStore } from "@reduxjs/toolkit";
+import shoppingReducer from "./utils/shoppingSlice";
+import { loadState, saveState } from "./utils/localStorage";
+
+// load the state from local storage if present
+const preloadedState = loadState();
 
 const store = configureStore({
-    reducer: {
-        shopping: shoppingReducer,
-    }
-})
+  reducer: {
+    shopping: shoppingReducer,
+  },
+  preloadedState,
+});
 
-export default store
+store.subscribe(() => {
+  saveState(store.getState());
+});
+
+// uncomment to remove local storage
+// localStorage.removeItem("appState");
+
+export default store;
