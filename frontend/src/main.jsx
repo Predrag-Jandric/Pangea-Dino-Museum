95c2cc928784440cb84973433345be0692ee0d8b
predrag jandric <predrag.jandric.bg@gmail.com>
2025-03-19 18:25:18 +0100

    will ADD: scroll to top button

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -6,11 +6,13 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import "./index.css";
 import App from "./App.jsx";
 import ShoppingCartPage from "./components/ShoppingCartPage";
+import ScrollToTopButton from "./utils/ScrollToTopButton.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <Provider store={store}>
       <Router>
+      <ScrollToTopButton />
         <Routes>
           <Route path="/" element={<App />} />
           <Route path="/shoppingCartPage" element={<ShoppingCartPage />} />
---
+++ frontend/src/utils/ScrollToTopButton.jsx
@@ -0,0 +1,39 @@
+import { useEffect, useState } from "react";
+import { GrLinkTop } from "react-icons/gr";
+
+export default function ScrollToTopButton() {
+  const [isVisible, setIsVisible] = useState(false);
+
+  useEffect(() => {
+    const toggleVisibility = () => {
+      if (window.pageYOffset > 600) {
+        setIsVisible(true);
+      } else {
+        setIsVisible(false);
+      }
+    };
+
+    window.addEventListener("scroll", toggleVisibility);
+
+    return () => window.removeEventListener("scroll", toggleVisibility);
+  }, []);
+
+  function handleJumpToTop(e) {
+    e.preventDefault();
+    window.scrollTo({ top: 0, behavior: "smooth" });
+  }
+
+  return (
+    <button
+      type="button"
+      onClick={handleJumpToTop}
+      className={`fixed bottom-10 right-1 z-50 rounded-full border border-primary bg-white p-3.5 text-black sm:hover:mb-1 shadow-md transition-all duration-300 hover:bg-white sm:hover:text-primary ${
+        isVisible ? "opacity-100" : "opacity-0"
+      }`}
+      aria-label="Scroll to top"
+      title="To top"
+    >
+      <GrLinkTop />
+    </button>
+  );
+}
